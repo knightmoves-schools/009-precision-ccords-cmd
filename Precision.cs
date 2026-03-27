c04d9a7d749122893126a211924429f2eab84a87
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-27 15:16:30 -0500

    Update Precision.cs

--- Precision.cs
+++ Precision.cs
@@ -2,6 +2,6 @@
 public class Precision
 {
     public int MaxInteger = 2147483647;
-    public decimal DecimalProduct = 2.53468 * 5.75956733;
+    public decimal DecimalProduct = (decimal)(2.53468 * 5.75956733);
     
 }
